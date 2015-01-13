3266f6ce833676d7e002f224fac5a2eb2363eedd
hikemate <achinthya94@gmail.com>
2015-01-13 21:29:35 +0530

    <hikeMate>

---
+++ HikeMateWebsite/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HikeMateWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult login()
+        {
+            return View();
+        }
+
+        public ActionResult trackRecord()
+        {
+            return View();
+
+        }
+
+        public ActionResult GetLocations()
+        {
+            var locations = new List<Models.Locations>();
+            using (hikeMateDBEntities context = new hikeMateDBEntities())
+            {
+                try
+                {
+                    var query = from category in context.Cordinates
+                                select new
+                                {
+                                    latitude = category.latitude,
+                                    longitude = category.longitude
+                                };
+
+                    foreach (var categoryInfo in query)
+                    {
+                        locations.Add(new Models.Locations(categoryInfo.latitude, categoryInfo.longitude));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    
+                }
+            }
+         /*   var locations = new List<Models.Locations>()
+            {
+                new Models.Locations("6.9344", "79.8428")
+            };*/
+
+            return Json(locations, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ HikeMateWebsite/Models/DBLocations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HikeMateWebsite.Models
+{
+    public class DBLocations
+    {
+        public int id { get; set; }
+        public string latitude { get; set; }
+        public string longitude { get; set; }
+    }
+}
